9d4c8d811e0729652e349781f7c4754b31d846b2
Opolskiy <Opolskiy@bpcbt.com>
2018-10-22 18:20:53 +0300

    Fixed addition operation.

--- src/main/java/day1/homework/Addition.java
+++ src/main/java/day1/homework/Addition.java
@@ -2,18 +2,18 @@ package day1.homework;
 
 public interface Addition {
     static int add(int a, int b) {
-        long result = a + b;
+        long result = (long) a + b;
 
         int finalResult = 0;
 
         if (result > Integer.MAX_VALUE) {
             finalResult = (int) (result - Integer.MAX_VALUE);
-            System.out.println("The result is more than max int value and modified to 0 plus difference.");
-        }
-
-        if (result < Integer.MAX_VALUE){
-            finalResult = (int)(result + Integer.MAX_VALUE);
-            System.out.println("The result is less than min int value and modified to 0 plus difference.");
+            System.out.println("The result is more than max int value (" + Integer.MAX_VALUE  + ") and modified to 0 plus difference = " + finalResult);
+        } else if (result < Integer.MIN_VALUE){
+            finalResult = (int)(result - Integer.MIN_VALUE);
+            System.out.println("The result is less than min int value (" + Integer.MIN_VALUE + ") and modified to 0 plus difference = " + finalResult);
+        } else {
+            finalResult = (int)result;
         }
 
         return finalResult;
