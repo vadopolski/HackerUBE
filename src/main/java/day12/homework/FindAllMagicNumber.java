fdd1203986930429b8430f668b747ced62efb297
Opolskiy <Opolskiy@bpcbt.com>
2018-12-10 11:05:01 +0300

    Refactoring.

--- src/main/java/day12/homework/FindAllMagicNumber.java
+++ src/main/java/day12/homework/FindAllMagicNumber.java
@@ -6,13 +6,10 @@ public class FindAllMagicNumber {
         for (int a = 1; a <= 9; a++) {
             for (int b = 0; b <= 9; b++) {
                 for (int c = 0; c <= 9; c++) {
-                    StringBuilder abcStr = new StringBuilder(String.valueOf(a));
-                    abcStr.append(String.valueOf(b));
-                    abcStr.append(String.valueOf(c));
-                    Integer abcInt = Integer.valueOf(abcStr.toString());
+                    Integer abcInt = Integer.valueOf(String.valueOf(a) + String.valueOf(b) + String.valueOf(c));
                     int other = abcInt - a * a - b * b - c * c;
                     if (other % 11 == 0)
-                        System.out.println("abc = " + abcStr);
+                        System.out.println("abc = " + abcInt);
                 }
             }
         }
