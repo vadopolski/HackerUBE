e88690ff905daf9bdb006aa3e5d0379bb5655540
Vadim Opolski <vaopolskij@gmail.com>
2019-01-17 14:24:37 +0300

    Added jdbc homework.

---
+++ src/main/java/dayJdbc/Title.java
@@ -0,0 +1,13 @@
+package dayJdbc;
+
+public class Title {
+    private String name;
+
+    public Title(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
